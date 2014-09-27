cab9539834770fbfc5e4826c307d24c25b9a950c
Jojatekok <jojatekokweb@gmail.com>
2014-09-27 16:53:14 +0200

    Added the missing 'ApiKeys.cs' file

---
+++ PoloniexApi.Net.Demo/ApiKeys.cs
@@ -0,0 +1,11 @@
+﻿namespace Jojatekok.PoloniexAPI.Demo
+{
+    struct ApiKeys
+    {
+        // WARNING: In order not to expose the changes of this file to git, please
+        //          run the following command every time you clone the repository:
+        // git update-index --assume-unchanged "PoloniexApi.Net.Demo\ApiKeys.cs"
+        internal const string PublicKey = "";
+        internal const string PrivateKey = "";
+    }
+}
